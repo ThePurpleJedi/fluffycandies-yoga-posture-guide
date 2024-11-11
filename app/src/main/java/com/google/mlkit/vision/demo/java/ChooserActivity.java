91ee06a4f6ac912bbde4ac59a61420c89b53f67f
thepurplejedi <raghav.7314@gmail.com>
2024-11-11 17:38:36 +0530

    Removed useless code

--- app/src/main/java/com/google/mlkit/vision/demo/java/ChooserActivity.java
+++ app/src/main/java/com/google/mlkit/vision/demo/java/ChooserActivity.java
@@ -1,31 +1,17 @@
 package com.google.mlkit.vision.demo.java;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import com.google.mlkit.vision.demo.R;
 
 /** Demo app chooser which allows you pick from all available testing Activities. */
 public final class ChooserActivity extends AppCompatActivity
     implements AdapterView.OnItemClickListener {
   private static final String TAG = "ChooserActivity";
-
-  private static final int[] DESCRIPTION_IDS =
-          new int[] {
-            R.string.desc_camerax_live_preview_activity,
-          };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -37,43 +23,6 @@ public final class ChooserActivity extends AppCompatActivity
 
   @Override
   public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-    Class<?> clicked = CLASSES[position];
-    startActivity(new Intent(this, clicked));
-  }
-
-  private static class MyArrayAdapter extends ArrayAdapter<Class<?>> {
-
-    private final Context context;
-    private final Class<?>[] classes;
-    private int[] descriptionIds;
-
-    MyArrayAdapter(Context context, int resource, Class<?>[] objects) {
-      super(context, resource, objects);
-
-      this.context = context;
-      classes = objects;
-    }
-
-    @SuppressLint("InflateParams")
-    @NonNull
-    @Override
-    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-      View view = convertView;
-
-      if (convertView == null) {
-        LayoutInflater inflater =
-            (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
-        view = inflater.inflate(android.R.layout.simple_list_item_2, null);
-      }
-
-      ((TextView) view.findViewById(android.R.id.text1)).setText(classes[position].getSimpleName());
-      ((TextView) view.findViewById(android.R.id.text2)).setText(descriptionIds[position]);
-
-      return view;
-    }
-
-    void setDescriptionIds() {
-      this.descriptionIds = ChooserActivity.DESCRIPTION_IDS;
-    }
+    startActivity(new Intent(this, CameraXLivePreviewActivity.class));
   }
 }
